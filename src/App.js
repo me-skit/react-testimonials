4983abdf9ebbd58d6737c4b003ee93db6424387d
Meme <meme_es@hotmail.com>
2022-06-01 18:37:22 -0600

    Add a Testimonial call into the App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import Testimonial from './Components/Testimonial';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='main'>
+        <h1>Aenean euismod risus pellentesque tellus condimentum egestas nec</h1>
+        <Testimonial
+          name='Emma Bostian'
+          contry='Sweden'
+          title='Software Engineer'
+          company='Spotify'
+          image='01'
+          testimonial='Aenean euismod risus pellentesque tellus condimentum egestas nec vel ligula. scelerisque quam quis velit maximus, ut suscipit risus euismod bibendum justo nec risus aliquet lobortis.'
+        />
+        <Testimonial
+          name='Jhon Doe'
+          contry='Sweden'
+          title='Software Engineer'
+          company='Amazon'
+          image='02'
+          testimonial='Euismod risus pellentesque tellus condimentum egestas nec vel ligula. scelerisque quam quis velit maximus, ut suscipit risus euismod bibendum justo nec risus aliquet lobortis.'
+        />
+        <Testimonial
+          name='Clara Lein'
+          contry='US'
+          title='Software Engineer'
+          company='Twitter'
+          image='03'
+          testimonial='Aenean euismod risus pellentesque tellus condimentum egestas nec vel ligula. scelerisque quam quis velit maximus, ut suscipit risus euismod bibendum justo nec risus aliquet lobortis.'
+        />
+        <Testimonial
+          name='Shawn Lee'
+          contry='US'
+          title='Software Engineer'
+          company='Google'
+          image='04'
+          testimonial='Risus pellentesque tellus condimentum egestas nec vel ligula. scelerisque quam quis velit maximus, ut suscipit risus euismod bibendum justo nec risus aliquet lobortis.'
+        />                        
+      </div>
     </div>
   );
 }
